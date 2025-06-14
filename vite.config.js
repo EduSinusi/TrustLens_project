add33927113b23cccba17aff70d6e54d012e8587
Edu Sinusi <esinusi32@gmail.com>
2025-06-15 01:55:11 +0800

    Remove build configuration and proxy settings for the PipWindow component in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,24 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"), // Main app entry
-        pip: path.resolve(__dirname, "src/pipEntry.jsx"), // PiP window entry
-      },
-      output: {
-        entryFileNames: "[name].js", // Output as main.js and pipEntry.js
-      },
-    },
-  },
   server: {
-    port: 3000,
-    proxy: {
-      "/pip-window": {
-        target: "http://localhost:3000",
-        rewrite: () => "/pip.html",
-      },
-    },
+    port: 3000
   },
 });
